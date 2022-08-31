3c39bcf184317b6ce02838d340c925f2970e28eb
Ann <efremenkova9533@gmail.com>
2022-08-31 21:51:26 +0300

    fixed import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Tabs from "@mui/material/Tabs"
 import Tab from "@mui/material/Tab"
